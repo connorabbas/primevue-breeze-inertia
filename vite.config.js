1ee46ceaa1ecf56c824e1ea70179c247454fa01e
Connor Abbas <abbasconnor@gmail.com>
2024-01-27 10:55:29 -0600

    upgrades, use static copy for theme folders

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
     plugins: [
@@ -15,6 +16,18 @@ export default defineConfig({
                     includeAbsolute: false,
                 },
             },
+        }),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "node_modules/primevue/resources/themes/lara-light-indigo",
+                    dest: "../themes",
+                },
+                {
+                    src: "node_modules/primevue/resources/themes/lara-dark-indigo",
+                    dest: "../themes",
+                },
+            ],
         }),
     ],
 });
