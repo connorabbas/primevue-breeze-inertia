6071915afa6e5d88f5dab0eadf4ad838ea77575d
Connor Abbas <abbasconnor@gmail.com>
2024-06-29 15:47:32 -0500

    run psr12 pint

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
