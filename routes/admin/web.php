67de93fe3a892672ab408c2eb43073147b9dcacb
Connor Abbas <abbasconnor@gmail.com>
2024-09-12 01:14:41 +0000

    route cleanup

--- routes/admin/web.php
+++ routes/admin/web.php
@@ -2,36 +2,33 @@
 
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Admin\Auth\AuthenticatedSessionController as AdminAuthenticatedSessionController;
 use App\Http\Controllers\Admin\Auth\NewPasswordController as AdminNewPasswordController;
 use App\Http\Controllers\Admin\Auth\PasswordResetLinkController as AdminPasswordResetLinkController;
 
 Route::prefix('admin')
     ->name('admin.')
     ->group(function () {
-        // Authentication
-        Route::controller(AuthenticatedSessionController::class)->group(function () {
-            Route::get('/login', 'create')
-                ->middleware(['guest', 'guest:admin'])
+        Route::middleware(['guest', 'guest:admin'])->group(function () {
+            Route::get('login', [AdminAuthenticatedSessionController::class, 'create'])
                 ->name('login');
-            Route::post('/login', 'store');
-            Route::post('/logout', 'destroy')
-                ->middleware(['auth:admin'])
-                ->name('logout');
+            Route::post('login', [AdminAuthenticatedSessionController::class, 'store']);
+            Route::get('forgot-password', [AdminPasswordResetLinkController::class, 'create'])
+                ->name('password.request');
+            Route::post('forgot-password', [AdminPasswordResetLinkController::class, 'store'])
+                ->name('password.email');
+            Route::get('reset-password/{token}', [AdminNewPasswordController::class, 'create'])
+                ->name('password.reset');
+            Route::post('reset-password', [AdminNewPasswordController::class, 'store'])
+                ->name('password.store');
         });
-        Route::get('forgot-password', [AdminPasswordResetLinkController::class, 'create'])
-            ->name('password.request');
-        Route::post('forgot-password', [AdminPasswordResetLinkController::class, 'store'])
-            ->name('password.email');
-        Route::get('reset-password/{token}', [AdminNewPasswordController::class, 'create'])
-            ->name('password.reset');
-        Route::post('reset-password', [AdminNewPasswordController::class, 'store'])
-            ->name('password.store');
 
-        // Authorized Routes
         Route::middleware(['auth:admin'])->group(function () {
             Route::get('/dashboard', function () {
                 return Inertia::render('Admin/Dashboard');
             })->name('dashboard');
+
+            Route::post('logout', [AdminAuthenticatedSessionController::class, 'destroy'])
+                ->name('logout');
         });
     });
