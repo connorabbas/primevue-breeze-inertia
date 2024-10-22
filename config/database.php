c0de5044eeba188d484d5ecb7049968be4020ba2
gregorypatton <75089595+gregorypatton@users.noreply.github.com>
2024-10-21 19:44:29 -0500

    Removed filament leftovers

--- config/database.php
+++ config/database.php
@@ -109,6 +109,26 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'legacy' => [
+            'driver' => 'mysql',
+            'url' => env('DB_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => 'legacy',
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => env('DB_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
     ],
 
     /*
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,35 @@
 
 namespace Database\Seeders;
 
+use Exception;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
         $this->command->info('Starting legacy data import process...');
+        try {
+            if (DB::connection('legacy')) {
 
-        $this->call([
-            TestUserSeeder::class,
-            LegacySupplierSeeder::class,
-            LegacyLocationSeeder::class,
-            LegacyPartSeeder::class,
-            LegacyProductSeeder::class,
-            LegacyGtinSeeder::class,
-            RelationshipSeeder::class,
-        ]);
+                $this->call([
+                    TestUserSeeder::class,
+                    LegacySupplierSeeder::class,
+                    LegacyLocationSeeder::class,
+                    LegacyPartSeeder::class,
+                    LegacyProductSeeder::class,
+                    LegacyGtinSeeder::class,
+                    RelationshipSeeder::class,
+                ]);
 
-        $this->command->info('Legacy data import process completed.');
+                $this->command->info('Legacy data import process completed.');
+            }
+        } catch (Exception $e) {
+            $this->command->error('Exception: ' . $e->getMessage());
+
+            Log::warning('Exception occurred: ', [$e]);
+        }
     }
 }
